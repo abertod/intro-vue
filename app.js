e949082ee7c430e30b50808b933c8b2af3c102b7
master <master@dib-e026-imac20.dib.upv.es>
2025-02-18 19:32:04 +0100

    Vue mediante CDN

---
+++ app.js
@@ -0,0 +1,11 @@
+//console.log(Vue);
+const { createApp, ref} = Vue;
+
+const app = createApp({
+    template: `
+    <h1>Hola Mundo</h1>
+    <h2>Desde app.js</h2>
+    `
+});
+
+app.mount('#myApp')
