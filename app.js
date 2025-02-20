d6e2be1ba06d7fd6dfa3cea1222573687ecf31a1
master <master@dib-e026-imac20.dib.upv.es>
2025-02-20 19:03:25 +0100

    Setup y ref

--- app.js
+++ app.js
@@ -3,9 +3,28 @@ const { createApp, ref} = Vue;
 
 const app = createApp({
     template: `
-    <h1>Hola Mundo</h1>
+    <h1>{{ nuntius }}.<br>Hola mundo</h1>
+    <h2>{{auctor}}</h2>
     <h2>Desde app.js</h2>
-    `
+    `,
+
+    setup() {
+
+        const nuntius = ref("I'm Bataman");
+
+        const auctor = ref('Batman');
+
+        setTimeout(()=> {
+            nuntius.value = 'Soy Goku.'
+            auctor.value = 'Goku. kakaroto';
+            console.log(nuntius);
+        }, 1000);
+
+        return{
+            nuntius, 
+            auctor,
+        }
+    }
 });
 
 app.mount('#myApp')
